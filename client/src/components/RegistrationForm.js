94660b90bfe06ccdbfa08f53dd38182e80d3f4cd
ankit_verma <ankiit3063@gmail.com>
2023-12-18 10:21:49 +0530

    Now form is rendering properly

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -19,16 +19,60 @@ const RegistrationForm = () => {
     }
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="name" value={formData.name} onChange={handleChange} />
-      
-      <button type="submit">Register</button>
-    </form>
+    <div className="form-container">
+      <form onSubmit={handleSubmit} className="registration-form">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="contact">Contact</label>
+          <input
+            type="text"
+            id="contact"
+            name="contact"
+            value={formData.contact}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="batch">Batch</label>
+          <select id="batch" name="batch" value={formData.batch} onChange={handleChange}>
+            <option value="">Select a batch</option>
+            <option value="6-7AM">6-7AM</option>
+            <option value="7-8AM">7-8AM</option>
+            <option value="8-9AM">8-9AM</option>
+            <option value="5-6PM">5-6PM</option>
+          </select>
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 };
 
--- server.js
+++ server.js
@@ -12,12 +12,37 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('your-mongodb-uri', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect('mongodb+srv://ankiit3063:tlOIhP6WJi7CxnEV@cluster-yoga.ulqvp4p.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 // Routes
 app.post('/register', async (req, res) => {
-    
-});
+    try {
+        // Basic validation
+        const { name, age, contact, batch } = req.body;
+        if (age < 18 || age > 65) {
+            return res.status(400).json({ message: 'Invalid age. Must be between 18 and 65.' });
+        }
+        if (!name || !contact || !batch) {
+            return res.status(400).json({ message: 'Missing required fields.' });
+        }
 
+        // Create new participant
+        const newParticipant = new Participant({ name, age, contact, batch });
+        await newParticipant.save();
+
+        // Create initial payment (assuming 500 Rs INR)
+        const newPayment = new Payment({
+            participantId: newParticipant._id,
+            amount: 500,
+            date: new Date() // Current date
+        });
+        await newPayment.save();
+
+        // Send success response
+        res.status(201).json({ message: 'Registration successful', participantId: newParticipant._id });
+    } catch (error) {
+        console.error('Registration Error:', error);
+        res.status(500).json({ message: 'Server error during registration' });
+    }
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
